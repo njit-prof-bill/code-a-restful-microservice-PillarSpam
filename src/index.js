b3cadad0800302de239995db301c0fd309087afd
Beet <markkotys123@gmail.com>
2024-10-18 12:00:20 -0400

    Completed REST API microservice

--- src/index.js
+++ src/index.js
@@ -5,13 +5,62 @@ const port = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// **************************************************************
-// Put your implementation here
-// If necessary to add imports, please do so in the section above
+var users = [];
+var id = 1;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/users', (req, res) =>{
+    const {name, email} = req.body;
+    if (!email || !name){
+        res.status(400).json({erro: "Fields required"})
+    }
+    else{
+        const user = {id: id++, name: name, email: email}
+        users.push(user);
+        res.status(201).json(user);
+    }
 });
+
+app.get('/users/:id', (req, res) => {
+    const ident = parseInt(req.params.id);
+    const user = users.find(x => x.id === ident);
+
+    if (!user){
+        return res.status(404).json({error: "User not found."});
+    }
+    res.status(200).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const ident = parseInt(req.params.id);
+    const {name, email} = req.body
+    const user = users.find(x => x.id === ident);
+
+    if (!user){
+        return res.status(404).json({error: "User not found."});
+    }
+    if (name){
+        user.name = name;
+    }
+    if (email){
+        user.email = email;
+    }
+
+    res.status(200).json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const ident = parseInt(req.params.id);
+    const userIndex = users.findIndex(x => x.id === ident);
+
+    if (userIndex === -1) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    users.splice(userIndex, 1);
+
+    res.status(204).send();
+});
+
 
 // Do not touch the code below this comment
 // **************************************************************
